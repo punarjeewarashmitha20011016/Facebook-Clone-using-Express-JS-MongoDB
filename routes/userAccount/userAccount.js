4e104c4e706ce925c585aabd55fb318da7b57cbf
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-08-22 14:12:41 +0530

    user login done

--- routes/userAccount/userAccount.js
+++ routes/userAccount/userAccount.js
@@ -1,28 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const Item = require("../../models/users.models")
+const User = require("../../models/users.models")
 
 router.get("/",async(req,resp)=>{
     try{
-        const item = await Item.find();
-        item.forEach(element => {
+        const user = await User.find();
+        user.forEach(element => {
             let password = Buffer.from(element.password, 'base64').toString('ascii');  
             element.password = password;
         });
-        resp.json(item);
+        resp.json(user);
     }catch(err){
         resp.json({"message : ":err});
     }
 })
 
-router.post("/",async(req,resp)=>{
-    const item = new Item(req.body);
+router.get("/login/:email/:password",async(req,resp)=>{
     try{
-        let password = item.password;
+        const email = req.params.email;
+        const password = req.params.password;
+        const user =await User.find();
+        user.forEach(async element => {
+            console.log("hi");
+            if(((element.email === email) & (Buffer.from(element.password, 'base64').toString('ascii') === password))){
+                const userLogin =await User.findById(element.id);
+                userLogin.password = Buffer.from(element.password,'base64').toString('ascii');
+                resp.json(userLogin);
+            }
+        });
+    }catch(err){
+        resp.json({"message : ":err})
+    }
+})
+
+router.post("/",async(req,resp)=>{
+    const user = new User(req.body);
+    try{
+        let password = user.password;
         let buff = new Buffer(password);
         let base64Data = buff.toString('base64');
-        item.password = base64Data;
-       const response = await item.save();
+        user.password = base64Data;
+        user.phoneNumber = 0+user.phoneNumber;
+       const response = await user.save();
        resp.json(response);
     }catch(err){
         resp.json({"message : ":err})
