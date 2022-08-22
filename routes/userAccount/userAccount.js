ff05409620d4b8848a30387d5c3a803e65e26893
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-08-22 13:54:03 +0530

    added user password encoded and decoded while post and get by base64

--- routes/userAccount/userAccount.js
+++ routes/userAccount/userAccount.js
@@ -5,6 +5,10 @@ const Item = require("../../models/users.models")
 router.get("/",async(req,resp)=>{
     try{
         const item = await Item.find();
+        item.forEach(element => {
+            let password = Buffer.from(element.password, 'base64').toString('ascii');  
+            element.password = password;
+        });
         resp.json(item);
     }catch(err){
         resp.json({"message : ":err});
@@ -14,6 +18,10 @@ router.get("/",async(req,resp)=>{
 router.post("/",async(req,resp)=>{
     const item = new Item(req.body);
     try{
+        let password = item.password;
+        let buff = new Buffer(password);
+        let base64Data = buff.toString('base64');
+        item.password = base64Data;
        const response = await item.save();
        resp.json(response);
     }catch(err){
